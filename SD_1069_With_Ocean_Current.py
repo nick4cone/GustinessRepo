dffe6abc7f031faa7716911a50dab9a665deb43e
nick4cone <ncf44@miami.edu>
2024-04-12 15:28:57 -0400

    COARE w/ ocean relative wind speed

--- SD_1069_With_Ocean_Current.py
+++ SD_1069_With_Ocean_Current.py
@@ -9,9 +9,8 @@ import numpy as np
 import xarray as xa
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.dates import date2num, num2date, DateFormatter
-from matplotlib.dates import DayLocator, HourLocator
-from sklearn.linear_model import LinearRegression
+from matplotlib.dates import date2num, DateFormatter
+from matplotlib.dates import HourLocator, MonthLocator
 from COARE.coare36vn_zrf_et import coare36vn_zrf_et  # from email thread
 from COARE.coare_no_ug_param import coare_no_ug_param
 
@@ -205,3 +204,98 @@ ax01.plot(date2num(SD.center_time)[0:n],  # UADCP 60 min mean
           label='HR Mean U Ocean')
 ax01.xaxis.set_major_locator(HourLocator())
 ax01.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+
+# %%
+# run COARE with no gustiness parameter
+coare_out = coare_no_ug_param(
+    u=SD.scalar_60min_mean_wind_speed,  # scalar mean wind speed
+    zu=5.2,
+    t=SD.mean60min.TEMP_AIR_MEAN.values,
+    zt=2.3,
+    rh=SD.mean60min.RH_MEAN.values,
+    zq=2.3,
+    P=np.nan_to_num(SD.mean60min.BARO_PRES_MEAN.values, nan=1015),  # fill nans
+    ts=SD.mean60min.TEMP_SBE37_MEAN.values,
+    sw_dn=SD.mean60min.SW_IRRAD_TOTAL_MEAN.values,
+    lw_dn=SD.mean60min.LW_IRRAD_MEAN.values,
+    lat=SD.mean60min.latitude.values,
+    lon=SD.mean60min.longitude.values,
+    jd=SD.center_time.to_julian_date().to_numpy(),
+    zi=600.0,
+    rain=np.full(len(SD.center_time), np.nan),  # nan array for rain
+    Ss=SD.mean60min.SAL_SBE37_MEAN.values)
+
+# hsb = sensible heat flux (W/m^2) ... positive for Tair < Tskin
+# hlb = latent heat flux (W/m^2) ... positive for qair < qs
+sensible = coare_out[:, 2]
+latent = coare_out[:, 3]
+thflx = sensible + latent  # positive cools the ocean (heats the atmosphere)
+
+# U_HR - U_Ocean_HR
+U_REL = SD.mean60min.UWND_MEAN.values - SD.mean60min.UADCP.values
+
+# V_HR - V_Ocean_HR
+V_REL = SD.mean60min.VWND_MEAN.values - SD.mean60min.VADCP.values
+
+# ocean relative vector mean wind speed
+vector_60min_ocean_relative = np.sqrt(U_REL ** 2 + V_REL ** 2)
+
+# run coare with parameterized gustiness ug
+coare_out_p = coare36vn_zrf_et(
+    u=SD.vector_60min_mean_wind_speed,
+    zu=5.2,
+    t=SD.mean60min.TEMP_AIR_MEAN.values,
+    zt=2.3,
+    rh=SD.mean60min.RH_MEAN.values,
+    zq=2.3,
+    P=np.nan_to_num(SD.mean60min.BARO_PRES_MEAN.values, nan=1015),  # fill nans
+    ts=SD.mean60min.TEMP_SBE37_MEAN.values,
+    sw_dn=SD.mean60min.SW_IRRAD_TOTAL_MEAN.values,
+    lw_dn=SD.mean60min.LW_IRRAD_MEAN.values,
+    lat=SD.mean60min.latitude.values,
+    lon=SD.mean60min.longitude.values,
+    jd=SD.center_time.to_julian_date().to_numpy(),
+    zi=600.0,
+    rain=np.full(len(SD.center_time), np.nan),  # nan array for rain
+    Ss=SD.mean60min.SAL_SBE37_MEAN.values)
+
+# gustiness parameter from COARE
+ug = coare_out_p[:, 47]
+SD_missing_wind_var = (SD.scalar_60min_mean_wind_speed ** 2 -
+                       SD.vector_60min_mean_wind_speed ** 2)
+
+# hsb = sensible heat flux (W/m^2) ... positive for Tair < Tskin
+# hlb = latent heat flux (W/m^2) ... positive for qair < qs
+sensible_p = coare_out_p[:, 2]
+latent_p = coare_out_p[:, 3]
+thflx_p = sensible_p + latent_p  # positive cools the ocean
+
+# %%
+# measured missing wind variance and ug^2
+fig02, ax02 = plt.subplots()
+
+# missing wind variance colored by SST
+sc02 = ax02.scatter(date2num(SD.center_time),
+                    SD_missing_wind_var,
+                    c=SD.mean60min.TEMP_SBE37_MEAN.values,
+                    s=1,
+                    cmap='bwr',
+                    linewidth=0.7,
+                    label=r"$\langle U^{2} + V^{2} \rangle"
+                    r"- (\langle U \rangle^2 + \langle V \rangle^2)"
+                    r"\: \langle\rangle_{60 \, min}$")  # raw string
+
+# ug^2
+ax02.plot(date2num(SD.center_time),
+          ug**2,
+          color="black",
+          linewidth=0.7,
+          label="$Ug^2$ from COARE")
+
+# properties
+ax02.xaxis.set_major_locator(MonthLocator())
+ax02.xaxis.set_major_formatter(DateFormatter('%b'))
+fig02.colorbar(sc02, label='SST (degC)')
+ax02.set_ylabel("$m^2 / s^2$")
+ax02.legend(loc='upper left', fontsize='small')
+ax02.set_title('Measured Missing Wind Variance & $Ug^2$')
